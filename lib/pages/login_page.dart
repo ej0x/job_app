f4e65c27bfa0cf4ccc094320a6fb46b20526ddbe
ej0x <ejas01@outlook.com>
2023-09-03 19:31:22 +0530

    icon and text added

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,34 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              // logo
+              Icon(
+                Icons.work,
+                size: 80,
+                color: Colors.grey[800],
+              ),
+
+              // welcome back message
+              const Text(
+                "WELCOME BACK",
+                style: TextStyle(fontSize: 25),
+              )
+              // email text fields
+
+              // password text fields
+
+              // sign in button
+
+              // not a member ? register now
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
