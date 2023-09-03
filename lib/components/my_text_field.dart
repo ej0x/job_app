ad924556eab5aeff131bd8d1e83b3f51c70e92f7
ej0x <ejas01@outlook.com>
2023-09-03 19:31:40 +0530

    text_field initial

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditing
+  const MyTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
