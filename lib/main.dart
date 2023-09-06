530edcd79d86db672650f0e5b3ce8e7a23ce41c0
ej0x <ejas01@outlook.com>
2023-09-06 21:45:50 +0530

    rmd unused

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:job_app/firebase_options.dart';
-import 'package:job_app/pages/login_page.dart';
-import 'package:job_app/pages/register_page.dart';
 import 'package:job_app/services/auth/auth_gate.dart';
-import 'package:job_app/services/auth/login_or_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
